a46d3e3ad01b139ac28bbf6c2637af66bd2adde0
Michelle Kusold <kusrox@gmail.com>
2015-04-01 11:09:24 -0400

    website backup apr 1

---
+++ js/amp.js
@@ -0,0 +1,19 @@
+amplitude_config = {
+            "amplitude_songs": [
+                {
+                    "name": "Checking things off a to-do list early in the morning",
+                    "artist": "Lullatone",
+                    "url": "audio/lullatone.mp3",
+                    "live": false,
+                }, 
+                {
+                    "name": "Finding a Leaf in your Girlfriend's Hair",
+                    "artist": "Lullatone",
+                    "url": "audio/lullatone2.mp3",
+                    "live": false,
+                }, 
+            ],
+            "amplitude_auto_play": true,
+            "amplitude_volume": 25,
+            "amplitude_continue_next": true
+        }
--- js/main.js
+++ js/main.js
@@ -1,22 +1,28 @@
-amplitude_config = {
-            "amplitude_songs": [
-                {
-                    "name": "Checking things off a to-do list early in the morning",
-                    "artist": "Lullatone",
-                    "url": "audio/lullatone.mp3",
-                    "live": false,
-                }, 
-                {
-                    "name": "Finding a Leaf in your Girlfriend's Hair",
-                    "artist": "Lullatone",
-                    "url": "audio/lullatone2.mp3",
-                    "live": false,
-                }, 
-            ],
-            "amplitude_auto_play": true,
-            "amplitude_volume": 25,
-            "amplitude_continue_next": true
-        }
 window.onload = function(){
-TweenLite.to("#shoe", 10, {rotation:"-360", transformOrigin:"0% 100%"});
+if (navigator.vendor.indexOf("Apple")==0 && /\sSafari\//.test(navigator.userAgent)){ //not fully supported on 1.7 jQuery
+    $("#Layer_1").css("position", "relative");
+    $("#Layer_1").css("height", "100%");
+    $("#Layer_1").css("width", "90%");
+    $("#bike_wrapper").css("width", "60%");
+    $("#bike_wrapper").css("height", "50%");
+    $("#bike_wrapper").css("bottom", "10px");
+    $("svg").css("bottom", "0");
+}
+// skrollr initialization
+
+skrollr.init({
+        forceHeight: false,
+        smoothScrolling: true,
+        easing: {
+            WTF: Math.random,
+            inverted: function(p) {
+                return 1-p;
+            }
+        }
+});
+
+$( "#scroller" ).click(function() {
+  skrollr.animateTo(400);
+});
+
 }
